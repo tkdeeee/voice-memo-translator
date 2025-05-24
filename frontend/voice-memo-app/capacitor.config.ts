75714b1ac09b899c3abe3537d5ae33bb94a6b898
tkdeeee <17320508tkd@gmail.com>
2025-05-24 19:52:40 +0900

    再編

--- frontend/voice-memo-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yourdomain.voicememo',
-  appName: 'voice-memo-app',
-  webDir: 'build'
-};
-
-export default config;
