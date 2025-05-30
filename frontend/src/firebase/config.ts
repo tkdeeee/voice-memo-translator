a7f0831a92205e8b4f23c303fc8a0ec9339d4c67
tkdeeee <17320508tkd@gmail.com>
2025-05-30 18:53:48 +0900

    firebase（authentication,firestore）の設定

---
+++ frontend/src/firebase/config.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCH37IFvMW5daEX5u6flyu7cWWcSlGl79k",
+  authDomain: "voice-to-shareablememo-app.firebaseapp.com",
+  projectId: "voice-to-shareablememo-app",
+  storageBucket: "voice-to-shareablememo-app.firebasestorage.app",
+  messagingSenderId: "280618739373",
+  appId: "1:280618739373:web:5687480b1b2d7c2de84824",
+  measurementId: "G-D63BP31QBL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export default {auth, db, app};
