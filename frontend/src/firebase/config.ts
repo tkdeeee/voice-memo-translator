2080fe5ddf211531b17f0153a58e066643b80043
tkdeeee <17320508tkd@gmail.com>
2025-06-30 03:45:31 +0900

    .env

--- frontend/src/firebase/config.ts
+++ frontend/src/firebase/config.ts
@@ -10,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCH37IFvMW5daEX5u6flyu7cWWcSlGl79k",
-  authDomain: "voice-to-shareablememo-app.firebaseapp.com",
-  projectId: "voice-to-shareablememo-app",
-  storageBucket: "voice-to-shareablememo-app.firebasestorage.app",
-  messagingSenderId: "280618739373",
-  appId: "1:280618739373:web:5687480b1b2d7c2de84824",
-  measurementId: "G-D63BP31QBL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
